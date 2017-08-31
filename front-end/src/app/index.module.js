9c33e9fb8138b5e7ea9aee57c3d94b005362f7a1
yuzhuo li <yuzhuoli@yuzhuos-MacBook-Pro.local>
2017-08-31 10:45:10 +0800

    add more sections for main page

--- front-end/src/app/index.module.js
+++ front-end/src/app/index.module.js
@@ -3,17 +3,28 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
+
 import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
 import { NavbarController } from './components/navbar/navbar.controller';
 import { FooterController } from './components/footer/footer.controller';
 import { TopContentController } from './components/topContent/topContent.controller';
+import { CenterContentController } from './components/centerContent/centerContent.controller';
+import { VideoSectionController } from './components/videoSection/videoSection.controller';
+import { CustomizedSectionController } from './components/customizedSection/customizedSection.controller';
+import { MoreSectionController } from './components/moreSection/moreSection.controller';
+
 import { CompareToDirective } from './directives/compareTo.directive';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
+
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { FooterDirective } from '../app/components/footer/footer.directive';
 import { TopContentDirective } from '../app/components/topContent/topContent.directive';
+import { CenterContentDirective } from '../app/components/centerContent/centerContent.directive';
+import { VideoSectionDirective } from '../app/components/videoSection/videoSection.directive';
+import { CustomizedSectionDirective } from '../app/components/customizedSection/customizedSection.directive';
+import { MoreSectionDirective } from '../app/components/moreSection/moreSection.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 
 angular.module('stlcFront', ['ui.router', 'ui.bootstrap', 'toastr', 'satellizer','pascalprecht.translate'])
@@ -30,8 +41,16 @@ angular.module('stlcFront', ['ui.router', 'ui.bootstrap', 'toastr', 'satellizer'
   .controller('NavbarController', NavbarController)
   .controller('FooterController', FooterController)
   .controller('TopContentController', TopContentController)
+  .controller('CenterContentController', CenterContentController)
+  .controller('VideoSectionController', VideoSectionController)
+  .controller('CustomizedSectionController', CustomizedSectionController)
+  .controller('MoreSectionController', MoreSectionController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeFooter', FooterDirective)
   .directive('acmeTopContent', TopContentDirective)
+  .directive('acmeCenterContent', CenterContentDirective)
+  .directive('acmeVideoSection', VideoSectionDirective)
+  .directive('acmeCustomizedSection', CustomizedSectionDirective)
+  .directive('acmeMoreSection', MoreSectionDirective)
   .directive('acmeMalarkey', MalarkeyDirective)
   .directive('compareTo', CompareToDirective);
